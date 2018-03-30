83246b3602c154b37f28e86ec72db51a2879f78a
docsquallor <36542731+docsquallor@users.noreply.github.com>
2018-03-30 14:50:21 +0200

    Create SequenceEquation.java

---
+++ SequenceEquation.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] permutationEquation(int[] p) {
+        int[] inv = new int[p.length + 1];
+        IntStream.rangeClosed(1, p.length).forEach(x -> inv[p[x - 1]] = x);
+        return IntStream.rangeClosed(1, p.length).map(x -> inv[inv[x]]).toArray();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] p = new int[n];
+        for(int p_i = 0; p_i < n; p_i++){
+            p[p_i] = in.nextInt();
+        }
+        int[] result = permutationEquation(p);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
+        }
+        System.out.println("");
+
+
+        in.close();
+    }
+}
