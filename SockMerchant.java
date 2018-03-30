8822fd48d7a66c61283ac703b9c574be27472e2e
docsquallor <36542731+docsquallor@users.noreply.github.com>
2018-03-30 15:23:08 +0200

    Create SockMerchant.java

---
+++ SockMerchant.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int sockMerchant(int n, int[] ar) {
+        return IntStream.of(ar).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).
+            entrySet().stream().mapToInt(entry -> (int)(entry.getValue()/2)).sum();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = sockMerchant(n, ar);
+        System.out.println(result);
+    }
+}
