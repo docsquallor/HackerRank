5bcd4cf8599281c643864a3d3f08a89f34ac908a
docsquallor <36542731+docsquallor@users.noreply.github.com>
2018-03-30 15:22:55 +0200

    Create DrawingBook.java

---
+++ DrawingBook.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int solve(int n, int p){
+        return (int)Math.min(Math.floor(p/2), Math.floor(n/2) - Math.floor(p/2));
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int p = in.nextInt();
+        int result = solve(n, p);
+        System.out.println(result);
+    }
+}
