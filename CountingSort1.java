c3f3915f9e4da04f9038f30774cde0503dde2e69
docsquallor <36542731+docsquallor@users.noreply.github.com>
2018-03-30 15:26:26 +0200

    Create CountingSort1.java

---
+++ CountingSort1.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] countingSort(int[] arr) {
+        int[] count = new int[100];
+        for(int x : arr)
+        {
+            count[x]++;
+        }
+        
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int arr_i = 0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        int[] result = countingSort(arr);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+
+
+        in.close();
+    }
+}
