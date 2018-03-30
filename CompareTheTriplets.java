86d846235886e097ccaf181737a3117118e4b299
docsquallor <36542731+docsquallor@users.noreply.github.com>
2018-03-30 15:29:46 +0200

    Create CompareTheTriplets.java

---
+++ CompareTheTriplets.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        return new int[]{unit(a0, b0) + unit(a1, b1) + unit(a2, b2), 
+                         unit(b0, a0) + unit(b1, a1) + unit(b2, a2)};
+    }
+    
+    static int unit(int x, int y)
+    {
+        return x > y ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+        
+
+    }
+}
