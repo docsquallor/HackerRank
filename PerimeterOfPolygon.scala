0379747c9684dbcf3fdd5b8bb984d555daef2832
Nikhil Jain <36542731+docsquallor@users.noreply.github.com>
2018-04-07 16:04:02 +0200

    Update PerimeterOfPolygon.scala

--- PerimeterOfPolygon.scala
+++ PerimeterOfPolygon.scala
@@ -4,11 +4,10 @@ object Solution {
         val sc = new java.util.Scanner(System.in)
         val points = sc.nextInt()
         var b = Stream.continually((sc.nextInt(), sc.nextInt())).take(points).toList
-        val h = b.head
-        b = b.tail :+ b.head
-        println(b.foldLeft((h._1, h._2, 0.0)){
-            case((x1, y1, acc), (x2, y2)) => (x2, y2, acc + math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2)))
-        }._3)
+        b = b :+ b.head
+        println(b.tail.foldLeft((b.head, 0.0)){
+            case((p, acc), (x2, y2)) => ((x2, y2), acc + math.sqrt(math.pow(x2 - p._1, 2) + math.pow(y2 - p._2, 2)))
+        }._2)
                            
     }
 }
